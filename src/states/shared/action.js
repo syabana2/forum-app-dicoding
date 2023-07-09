c84f415bf859f3a26bcbe0ed198f70d8b3f9be94
Rizki_Syaban <rizkisyaban2@gmail.com>
2023-07-09 16:43:44 +0700

    make test pass

--- src/states/shared/action.js
+++ src/states/shared/action.js
@@ -38,7 +38,7 @@ function asyncRegisterUserAndThenLogin({name, email, password}) {
       response = {success: true};
     } catch (error) {
       alert(error.message);
-      response = {success: true};
+      response = {success: false};
     }
 
     dispatch(hideLoading());
